ac730a45b83f55bc914bd4a2a7b32c88a989e53c
Shubham <shubham710mishra@gmail.com>
2019-03-15 21:13:08 +0530

    post page part C almost done

---
+++ Project_Stub_File/stub/js/post.js
@@ -0,0 +1,72 @@
+var likes =0;
+function liker() {
+    if(likes===0)
+    {
+      document.getElementById("likebtn").innerHTML ='<i class="fas fa-thumbs-up"></i>&nbsp;Liked!';    
+        likes++;
+        document.getElementById("liketxt").innerHTML = likes+' person liked this!';
+    }
+    else 
+    {
+            likes++;
+        document.getElementById("liketxt").innerHTML = likes +' people have liked this!';
+    }
+}
+function comments(cmt){
+    var par = document.createElement("div");
+    var lis = document.createTextNode(cmt.value);
+   par.appendChild(lis);
+   // par.prepend(lis);
+    var vs = document.getElementById("listofcomments");
+    //vs.appendChild(par);
+    vs.prepend(par);
+    par.classList.add("cmntdiv");
+    vs.style.padding ="10px 20px 20px 20px";
+}
+var editone=1;
+function editor(eds)
+{
+    var vs = document.getElementById("jimiedit");
+    
+    var ds = document.getElementById("blogTitle");
+    
+    if(editone===1){
+    var fun = document.getElementById("blogBody").innerHTML;
+    var fun1 = document.getElementById("blogTitleNew").innerHTML;
+
+    document.getElementById("blogBody").setAttribute('style','display:none;');
+    document.getElementById("blogTitleNew").setAttribute('style','display:none;');    
+    var par = document.createElement('textarea');
+    var par1 = document.createElement('textarea');
+    var lis = document.createTextNode(fun.trim());
+    var lis1 = document.createTextNode(fun1.trim());
+    par.appendChild(lis);
+    par1.appendChild(lis1);
+    vs.appendChild(par);
+    ds.appendChild(par1);
+    par.classList.add("editcmnt1");
+    par1.classList.add("editcmnt2");
+  par.style.height = (par.scrollHeight)+"px";
+      par1.style.height = (par1.scrollHeight-25)+"px";
+        document.getElementById("editbutton").innerHTML ='Save&nbsp;<i class="fas fa-save"></i>'; 
+    editone=2;
+        
+        document.getElementsByClassName("blogAuthor")[0].style.marginBottom="10px";
+    }
+    else {
+       var gun = document.getElementsByClassName("editcmnt1")[0].value;
+        var gun1 = document.getElementsByClassName("editcmnt2")[0].value;
+       console.log(gun);
+        document.getElementById("blogBody").innerHTML =gun;
+        document.getElementById("blogTitleNew").innerHTML =gun1;
+        document.getElementsByClassName("editcmnt1")[0].setAttribute('style','display:none;');
+        document.getElementsByClassName("editcmnt2")[0].setAttribute('style','display:none;');
+        document.getElementById("blogBody").setAttribute('style','display:block;');
+        document.getElementById("blogTitleNew").setAttribute('style','display:block;');
+         document.getElementById("editbutton").innerHTML ='Edit&nbsp;<i class="fas fa-edit"></i>';
+        editone=1;
+        vs.removeChild(vs.lastChild);
+        ds.removeChild(ds.lastChild);
+    }
+        
+}
